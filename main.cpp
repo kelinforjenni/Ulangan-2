733935fe34632927efee1d2ab956ef72256eb7e5
kelinforjenni <kelinforjenni@gmail.com>
2024-08-07 14:48:23 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <cstdlib>
+    #include <iostream>
+    using namespace std;
+    
+    int main()
+    {
+        cout << "Kelompok Blay";
+        cin.ignore();
+        cout << endl;
+        cout << "NIS" << endl;
+        cout << "1. 2341019\n"
+            "2. 2341019\n"
+            "3. 23241026\n"
+            "4. 23241098" << endl;
+        cin.ignore();
+        cout << "Nama" << endl;
+        cout << "1. Caroline Irwanda Susanto\n"
+            "2. Daffa Haifa Kamil\n"
+            "3. Jennifer Whitney Milano\n"
+            "4. Valencia" << endl;
+        cin.ignore();
+        cout << "Kelas 11.2" << endl;
+        cin.ignore();
+        cout << "Jurusan IPA" << endl;
+        cin.ignore();
+        cout << "Jenis Kelamin" << endl;
+        cout << "1. Caroline Irwanda Susanto (P)\n"
+            "2. Daffa Haifa Kamil (P)\n"
+            "3. Jennifer Whitney Milano (P)\n"
+            "4. Valencia (P)" << endl;
+        cin.ignore();
+        cout << "Alamat" << endl;
+        cout << "1. Caroline : Jalan Sawo No.2\n"
+            "2. Daffa : Pesona Griya Indah\n"
+            "3. Jennifer : Green Garden\n"
+            "4. Valencia : Jl.Kosambi Telagasari" << endl;
+        system("PAUSE");
+        return 0;
+    }
+ 
